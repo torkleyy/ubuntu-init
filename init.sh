7abd8e593eb525592df7c913765a161192d7251c
Thomas Schaller <torkleyy@gmail.com>
2020-06-03 14:48:55 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ set -e
 
 apt-get update -yq
 apt-get upgrade -yq
-apt-get install -yq git
+apt-get install -yq git vim
 
 curl -fsSL https://get.docker.com | sh
 apt-get install -yq docker-compose
@@ -12,7 +12,7 @@ apt-get install -yq docker-compose
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-MY_VIM_CONFIG = "autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab
+export MY_VIM_CONFIG="autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab
 autocmd FileType yml setlocal ts=2 sts=2 sw=2 expandtab"
 
 echo "${MY_VIM_CONFIG}" >> ~/.vim_runtime/my_configs.vim
